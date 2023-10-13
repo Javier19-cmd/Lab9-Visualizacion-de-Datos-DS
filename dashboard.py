82be70f4a8028b7c63cb1b5336ccc0d6474e2cdd
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2023-10-13 17:02:00 -0600

    avances

--- dashboard.py
+++ dashboard.py
@@ -16,23 +16,35 @@ from wordcloud import WordCloud
 from textblob import TextBlob
 from sklearn.linear_model import LogisticRegression
 from textblob import TextBlob
+import streamlit as st
+import numpy as np
+import time
 
-# Cargar el dataset desde un archivo CSV
+# Cargando el dataset desde un archivo CSV
 
 train = pd.read_csv("train.csv")
+
+# Creando un grid para enseñar las transformaciones
+st.title("Transformaciones del Dataset")
+col1, col2 = st.beta_columns(2)
+
+# Enseñando el DataFrame original
+with col1:
+    st.write("**DataFrame Original**")
+    st.write(train.head())
 
 # Cargando las primeras 5 filas del dataset.
 
 print("train")
 print(train.head())
 
-# Convertir el texto a mayúsculas o minúsculas.
-
 # Convertiendo todas las columnas de texto a minúsculas del dataset train.
 train = train.apply(lambda x: x.astype(str).str.lower() if x.dtype == "object" else x)
 
-# Imprimiendo el DataFrame resultante
-print(train)
+# Enseñando el DataFrame con texto en minúsculas
+with col2:
+    st.write("**DataFrame con Texto en Minúsculas**")
+    st.write(train.head())
 
 # Función para limpiar los caracteres especiales
 def clean_text(text):
@@ -59,3 +71,56 @@ location_unique_count = train['location'].nunique()
 # Imprimiendo las estadísticas de longitud de textos y la cantidad de categorías únicas en la ubicación
 print("Estadísticas de longitud de textos:\n", text_stats)
 print("\nCantidad de categorías únicas en base al texto:", location_unique_count)
+
+# Coloca aquí el código de procesamiento de datos que deseas combinar
+train['tweet_length'] = train['keyword'].apply(len)
+text_stats = train['tweet_length'].describe()
+location_unique_count = train['location'].nunique()
+
+# Gráfico de histograma de longitudes de textos excluyendo NaN
+fig_text_length = plt.figure(figsize=(10, 6))
+sns.histplot(train['tweet_length'].dropna(), bins=30, kde=True, color="steelblue")
+plt.title('Distribución de Longitudes de Textos')
+plt.xlabel('Longitud de Textos')
+plt.ylabel('Frecuencia')
+
+# Calcular la frecuencia de las palabras clave
+keyword_freq = train['keyword'].value_counts()
+
+# Seleccionar las palabras clave más frecuentes.
+top_keywords = keyword_freq.head(30)
+
+# Crear un gráfico de barras de las palabras clave más frecuentes
+fig_top_keywords = plt.figure(figsize=(10, 6))
+sns.barplot(x=top_keywords.values, y=top_keywords.index, color="darkred")
+plt.title('Palabras Clave Más Frecuentes')
+plt.xlabel('Frecuencia')
+plt.ylabel('Palabra Clave')
+
+# Sección de la aplicación Streamlit
+st.title("Laboratorio 9")
+
+# Barra de progreso y gráfico
+progress_bar = st.sidebar.progress(0)
+status_text = st.sidebar.empty()
+last_rows = np.random.randn(1, 1)
+#chart = st.line_chart(last_rows)
+
+for i in range(1, 101):
+    new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+    status_text.text("%i%% Complete" % i)
+    #chart.add_rows(new_rows)
+    progress_bar.progress(i)
+    last_rows = new_rows
+    time.sleep(0.05)
+
+progress_bar.empty()
+
+# Streamlit widgets automatically run the script from top to bottom. Since
+# this button is not connected to any other logic, it just causes a plain
+# rerun.
+st.button("Re-run")
+
+# Visualizar las gráficas generadas en la sección de procesamiento de datos
+st.pyplot(fig_text_length)
+st.pyplot(fig_top_keywords)
